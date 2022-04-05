57ca2ad30829682c185a32ee0ce6192613183a67
Jamshaid Khalid <jamshaidkhalid32@gmail.com>
2022-04-05 18:18:32 +0500

    wordcount

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 
 export default function TextForm(props) {
@@ -12,18 +12,43 @@ export default function TextForm(props) {
     setText(text.toUpperCase());
   }
 
+  const handleLoClick = () => {
+    // console.log("Up clicked");
+    setText(text.toLowerCase());
+  }
+
   const handleOnChage = (event) => {
     console.log("On change");
     setText(event.target.value);
   }
   return (
-    <div>
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <textarea className="form-control" value={text} onChange={handleOnChage} id="exampleFormControlTextarea1" rows="8"></textarea>
+    <>
+      <div className='container'>
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea className="form-control" value={text} onChange={handleOnChage} id="exampleFormControlTextarea1" rows="8"></textarea>
+        </div>
+
+        <button className='btn btn-primary mx-2' onClick={handleUpClick}>Convert to Uppercase</button>
+        <button className='btn btn-primary mx-2' onClick={handleLoClick}>Convert to Lowercase</button>
       </div>
 
-      <button className='btn btn-primary' onClick={handleUpClick}>Convert to Uppercase</button>
-    </div>
+
+
+      <div className='container mt-3'>
+        <h2>Your text summary</h2>
+        <p>{text.split(" ").length} words and {text.length} character</p>
+        {/* text.split(" ") gives the arrays of words which are separated at space */}
+        {/* this is really amazing. It is a hook. It is performed without reloading the page */}
+
+        {/* suppose 0.008 minutes are required to read one word */}
+        <p>{0.008 * text.split(" ").length} minutes to read</p>
+
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
+
+
+    </>
   )
 }
