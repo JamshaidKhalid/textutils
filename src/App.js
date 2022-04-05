b4133ea1d03d8e19c17026a6636f933c3b05f39d
Jamshaid Khalid <jamshaidkhalid32@gmail.com>
2022-04-05 23:37:59 +0500

    done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import Navbar from './components/Navbar.js';
 import TextForm from './components/TextForm';
+import {useState} from 'react';
 
 function App() {
+  //defalut mode is light and it can be only changed by setMode function
+  //we will pass this state to Navbar component as props
+  const [mode, setMode] = useState('light');
+
+
   return (
     <>
       {/* passing props to Navbar */}
-      <Navbar title="Textutils"/>
+      <Navbar title="Textutils" mode={mode}/>
 
 
       {/* container is a bootstrap class, without this the textform was not looking good */}
