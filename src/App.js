09724d50b6c4cfce92ff9edfc244b41607bb2d43
Jamshaid Khalid <jamshaidkhalid32@gmail.com>
2022-04-05 16:41:57 +0500

    textforms done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar.js';
+import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
       {/* passing props to Navbar */}
       <Navbar title="Textutils"/>
+      <div className='container'>
+        <TextForm/>
+      </div>
 
 
     </>
