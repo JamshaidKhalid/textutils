72bc67061749e752c7c14e4bff7df8b21c88dfff
Jamshaid Khalid <jamshaidkhalid32@gmail.com>
2022-04-05 17:55:53 +0500

    uppercase done

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -8,13 +8,19 @@ export default function TextForm(props) {
   //this updates the content without reloading the page
 
   const handleUpClick = () => {
-    
+    // console.log("Up clicked");
+    setText(text.toUpperCase());
+  }
+
+  const handleOnChage = (event) => {
+    console.log("On change");
+    setText(event.target.value);
   }
   return (
     <div>
       <h1>{props.heading}</h1>
       <div className="mb-3">
-        <textarea className="form-control" defaultValue={text} id="exampleFormControlTextarea1" rows="8"></textarea>
+        <textarea className="form-control" value={text} onChange={handleOnChage} id="exampleFormControlTextarea1" rows="8"></textarea>
       </div>
 
       <button className='btn btn-primary' onClick={handleUpClick}>Convert to Uppercase</button>
