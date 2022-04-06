3fb1df726c8af3c640e3ee43eeadcd42c0e836aa
Jamshaid Khalid <jamshaidkhalid32@gmail.com>
2022-04-06 23:28:04 +0500

    done

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -45,10 +45,7 @@ export default function TextForm(props) {
   }
 
   const handleCopyClick = () => {
-    let text = document.getElementById("myBox");
-    console.log(text)
-    text.select();
-    navigator.clipboard.writeText(text.value);
+    navigator.clipboard.writeText(text);
   }
 
   const handleExtraSpaces = () => {
@@ -72,24 +69,24 @@ export default function TextForm(props) {
           <textarea className="form-control" value={text} onChange={handleOnChage} id="myBox" style={{backgroundColor: props.mode === 'light' ? 'white' : '#04060d', color: props.mode === 'light' ? 'black' : 'white'}} placeholder='Enter your text here' rows="8"></textarea>
         </div>
 
-        <button className='btn btn-primary mx-2' onClick={handleUpClick}>Convert to Uppercase</button>
-        <button className='btn btn-primary mx-2' onClick={handleLoClick}>Convert to Lowercase</button>
-        <button className='btn btn-primary mx-2' onClick={handleInverseClick}>Convert to InVeRsE CaSe</button>
-        <button className='btn btn-primary mx-2' onClick={handleCopyClick}>Copy to Clipboard</button>
-        <button className='btn btn-primary mx-2' onClick={handleExtraSpaces}>Remove Extra Spaces</button>
-        <button className='btn btn-danger mx-2' onClick={handleClearClick}>Clear Text</button>
+        <button disabled={text.length===0} className='btn btn-primary mx-2 my-1' onClick={handleUpClick}>Convert to Uppercase</button>
+        <button disabled={text.length===0} className='btn btn-primary mx-2 my-1' onClick={handleLoClick}>Convert to Lowercase</button>
+        <button disabled={text.length===0} className='btn btn-primary mx-2 my-1' onClick={handleInverseClick}>Convert to InVeRsE CaSe</button>
+        <button disabled={text.length===0} className='btn btn-primary mx-2 my-1' onClick={handleCopyClick}>Copy to Clipboard</button>
+        <button disabled={text.length===0} className='btn btn-primary mx-2 my-1' onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+        <button disabled={text.length===0} className='btn btn-danger mx-2 my-1' onClick={handleClearClick}>Clear Text</button>
       </div>
 
 
 
       <div className={`container mt-3 text-${props.mode === 'light' ? 'dark': 'light'}`}>
         <h2>Your text summary</h2>
-        <p>{text.split(" ").length-1} words and {text.length} character</p>
+        <p>{text.split(/\s+/).filter((element) => {return element.length !== 0}).length} words and {text.length} character</p>
         {/* text.split(" ") gives the arrays of words which are separated at space */}
         {/* this is really amazing. It is a hook. It is performed without reloading the page */}
 
         {/* suppose 0.008 minutes are required to read one word */}
-        <p>{0.008 * (text.split(" ").length-1)} minutes to read</p>
+        <p>{0.008 * (text.split(/\s+/).filter((element) => {return element.length !== 0}).length)} minutes to read</p>
 
         <h2>Preview</h2>
         <p>{text.length > 0 ? text : "Enter text in above box to preview here"}</p>
